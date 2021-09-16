e1d5555408261d8ecc9568c437519327a2f29a9f
Astignoob <65780570+Astignoob@users.noreply.github.com>
2021-09-16 12:34:13 +0800

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -12,7 +12,6 @@ sudo service xrdp start
 apt-get install -y xfonts-100dpi xfonts-100dpi-transcoded xfonts-75dpi xfonts-75dpi-transcoded xfonts-base
 apt-get install -y tightvncserver
 tightvncserver :1
-echo XRDP Address: "https://dashboard.ngrok.com/endpoints/status"
 echo "===================================="
-echo "More  Free VPS akuh.net"
+echo "server is running"
 echo "===================================="
